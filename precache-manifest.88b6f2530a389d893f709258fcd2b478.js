21fdf65bca38704c51157dec587b1b6e662efbbf
facundof1 <facundof1@hotmail.com.ar>
2019-10-02 20:51:34 -0300

    Updates

---
+++ precache-manifest.88b6f2530a389d893f709258fcd2b478.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a6d54367c0acfbf674a8ec59fd986e",
+    "url": "/search-movie/index.html"
+  },
+  {
+    "revision": "59374f9011c9eb2ff4a5",
+    "url": "/search-movie/static/js/2.d88e76b6.chunk.js"
+  },
+  {
+    "revision": "345695dfddacd9e3249b",
+    "url": "/search-movie/static/js/main.8d1e684b.chunk.js"
+  },
+  {
+    "revision": "a5de3b273bc2c1bff10b",
+    "url": "/search-movie/static/js/runtime-main.fe2440f9.js"
+  }
+]);
